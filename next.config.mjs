747531036a65a05255543f0e3f7b019838356864
carloslucer <lucero.carlos.ch@gmail.com>
2026-02-11 12:12:57 -0300

    forzado redirecion a la pai

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    }, // o el puerto real que estés usando
+    async rewrites() {
+        // Usamos la variable del .env o un valor por defecto
+        const backendUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${backendUrl}/api/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
