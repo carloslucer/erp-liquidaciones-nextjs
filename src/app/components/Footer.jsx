ced2596f1bf42a2732a2eeee161e0eebe4ec1fda
carloslucer <lucero.carlos.ch@gmail.com>
2026-02-11 12:33:00 -0300

    modif footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
-export default function Footer({ dark = false }) {
+export default function Footer() {
   return (
-    <footer className={`py-3 mt-auto ${dark ? 'text-white' : 'text-gray-700'} `}>
+    <footer className="py-3 mt-auto bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4">
         <p className="text-center text-xs font-medium">
           Ministerio de Economía - Dirección General de Cómputos
